b5f3e673b1171a44b818e9efc619d0bee6059466
Suraj Kumar <itsksuraj2002@gmail.com>
2025-01-10 18:34:03 +0530

    initial

---
+++ src/data/constants.js
@@ -0,0 +1,290 @@
+export const Bio = {
+  name: "Suraj Kumar",
+  roles: [
+    "Software Developer",
+    "Frontend Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Suraj25122002",
+  resume:
+    "https://drive.google.com/file/d/1MFxAYI75vqEvKnfebdMBpWMCLhZB7LZX/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/suraj-kumar-a13568206/",
+  twitter: "https://twitter.com/SurajKumar",
+  insta: "https://www.instagram.com/suraj_gupta6_/?hl=en",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "JQuery",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Logo_jQuery.svg",
+      },
+
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "C#",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/C_Sharp_Logo_2023.svg/220px-C_Sharp_Logo_2023.svg.png",
+      },
+      {
+        name: ".NET CORE",
+        image:
+          "https://learn.microsoft.com/media/logos/logo_net.svg",
+      },
+      {
+        name: "SQL Server",
+        image: "https://www.sqlservertutorial.net/wp-content/uploads/sql-server-tutorial.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "WEB APIS",
+        image:
+          "https://solutionsuggest.com/wp-content/uploads/2021/11/Top-10-Free-Image-Generation-APIs.jpg",
+      },
+      {
+        name: "REST API",
+        image: "https://www.seobility.net/en/wiki/images/thumb/f/f1/Rest-API.png/450px-Rest-API.png",
+      },
+      {
+        name: "Microsoft Office",
+        image:
+          "https://t3.ftcdn.net/jpg/09/87/70/48/360_F_987704828_FqPyt9QHPsz2bVIkKNTObgtJ1SPnIcLq.jpg",
+      },
+      
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://t4.ftcdn.net/jpg/09/06/28/15/360_F_906281507_90fDRAp2bp8WftGkvZHEo0QqjnS2O4wr.jpg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/97/Netlify_logo_(2).svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: ".Net Developer Intern:",
+    company: "Sukrit infotech",
+    date: "Jan 2024 - April 2024",
+    desc: "Assisted in developing, testing, and maintaining .NET applications. Collaborated with senior developers to understand and implement project requirements. Wrote clean, efficient code in C# and .NET Core.",
+    skills: [
+      "C#",
+      ".NET CORE",
+      "SQL SERVER",
+      "WEB APIS",
+      "RESTFUL API",
+      "AUTHENTICATION & AUTHORIZATION",
+      "DOCKER",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Web Development Intern",
+    company: "Internshala",
+    date: "June 2023 - August 2023",
+    desc: "Developed a responsive web application for E-Commerce (Fashion Queen) using technologies: HTML, CSS, JavaScript, Bootstrap and jQuery.",
+    skills: [
+      "HTML5",
+      "CSS3",
+      "JAVASCRIPT",
+      "JQUERY",
+      "BOOTSTRAP",
+      "PHP",
+    ],
+    
+  },
+
+  
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "SQL Server Intern",
+    company: "INCAPP",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://iilm.edu.in/wp-content/uploads/2018/08/G-Noida-Campus.jpg",
+    school: "IILM Academy of Higher Learning, College of Engineering & Technology, Greater Noida, UP",
+    date: "Oct 2020 - June 2024",
+    grade: "6.91 CGPA",
+    desc: "I have completed Bachelor's degree in Computer Science and Engineering at IILM Academy of Higher Learning, College of Engineering & Technology, Greater Noida.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/commons/e/ec/1st_High_school_of_Argos,_Greece.jpg",
+    school: "S.B.S.Inter College, Sanibasantpur, Siwan, Bihar",
+    date: "Apr 2019 - March 2020",
+    grade: "60.2%",
+    desc: "I completed my class 12 high school education at S.B.S.Inter College, Sanibasantpur, Siwan, Bihar, where I studied Science.",
+    degree: "High School(XII), Science",
+  },
+  {
+    id: 2,
+    img: "https://rscsnachap.edu.in/images/campus.jpg",
+    school: "Riddhi Siddhi Central School, Nachap, Chapra, Saran, Bihar",
+    date: "Apr 2017 - Apr 2018",
+    grade: "64.8%",
+    desc: "I completed my class 10 education at Riddhi Siddhi Central School, Nachap, Chapra, Saran, Bihar.",
+    degree: "Matriculation(X), Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "To-Do List",
+    description:
+      "A To-Do List Project is an app that helps users organize tasks, set priorities, and track progress through features like task creation, due dates, reminders, and status updates. It can be developed for web or mobile platforms with optional collaboration and analytics features.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "Python",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Django",
+      "React Js",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 1,
+    title: "Portfolio",
+    description:
+      "A dynamic portfolio showcasing innovative web design and development, highlighting responsive layouts and user-centric interfaces. Each project emphasizes creativity, functionality, and seamless user experience across various platforms.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+    tags: ["React Js", "Bootstrap", "JavaScipt"],
+    category: "web app",
+    github: "",
+    webapp: "",
+    
+  },
+  {
+    id: 2,
+    title: "Online Library Management",
+    description:
+      "An efficient online library management system that streamlines book cataloging, user access, and borrowing processes. It offers a user-friendly interface with advanced search features and real-time inventory tracking.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: [
+      "Python",
+      "Django",
+      "SQlite",
+      "HTML",
+      "CSS",
+      "JavaScripts",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 3,
+    title: "Online Job Portal",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "A job seeker has to struggle a lot in searching the job portals of hundreds of companies. So, we have our Online Job Portal Project which is a free portal where people seeking jobs can apply their resumes/CVs for different jobs and can get hired via our platform.",
+    image:
+      "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+    tags: ["C#", ".Net Core", "Sql Server", "ORM", "Dependency Injection", "Automapper"],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 4,
+    title: "Movies & Series App",
+    description:
+      "A feature-rich Movies & Series app that offers seamless streaming, personalized recommendations, and easy navigation through genres, ratings, and release dates. Users can track, rate, and manage their watchlist effortlessly.",
+    image:
+      "https://camo.githubusercontent.com/fbf405040ae86b5d4a40f24e4ac034982cb8c0e2d850560ba64527997b258be8/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
+    tags: ["React Js", "HTML", "CSS", "JavaScript"],
+    category: "Web app",
+    github: "",
+    webapp: "",
+  },  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
---
+++ src/utils/Themes.js
@@ -0,0 +1,51 @@
+// export const darkTheme = {
+//     bg:"#757575",
+//     bgLight: "#1C1E27",
+//     primary:"#854CE6",
+//     text_primary:"#F2F3F4",
+//     text_secondary:"#b1b2b3",
+//     card:"#171721",
+//     card_light: '#191924',
+//     button:"#854CE6",
+//     white:"#FFFFFF",
+//     black:"#000000",
+// }
+
+// export const lightTheme = {
+//     bg:"#FFFFFF",
+//     bgLight: "#f0f0f0",
+//     primary:"#be1adb",
+//     text_primary:"#111111",
+//     text_secondary:"#48494a",
+//     card:"#FFFFFF",
+//     button:"#5c5b5b",
+// }
+
+export const darkTheme = {
+    bg: "#002F4D",            // Deep ocean blue (rich and dark background)
+    bgLight: "#4A90A4",       // Soft teal for lighter section backgrounds
+    primary: "#1D5B63",       // Deep sea green for calming accents
+    text_primary: "#E3F6F5",  // Light aqua for text to evoke water clarity
+    text_secondary: "#A1D4D1", // Subtle seafoam green for secondary text
+    card: "#014F86",          // Navy blue for cards to mimic the ocean depths
+    card_light: "#64A1C0",    // Lighter blue-green for hover/active state
+    button: "#1D5B63",        // Deep sea green for buttons (matching primary)
+    white: "#FFFFFF",         // White for highlights, mimicking foam and surf
+    black: "#000000",         // Black for deep contrast
+}
+
+export const lightTheme = {
+    bg: "#E1F5FE",            // Light ocean blue for background (gentle and airy)
+    bgLight: "#B2EBF2",       // Soft turquoise for lighter section backgrounds
+    primary: "#1D5B63",       // Deep sea green for accents
+    text_primary: "#111111",  // Dark text for strong contrast and readability
+    text_secondary: "#48494A", // Soft gray for secondary text
+    card: "#64B5F6",          // Light blue for cards (reminiscent of the sky)
+    card_light: "#81D4FA",    // Lighter shade of blue for hover/active state
+    button: "#0288D1",        // Bright ocean blue for buttons
+    white: "#FFFFFF",         // White for highlights
+    black: "#000000",         // Black for deep contrast
+}
+
+
+
